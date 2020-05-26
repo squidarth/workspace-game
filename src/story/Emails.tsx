6fa43e197c592f9086d16fcfd3e67af068299bd1
Sid Shanker <sshanker220@gmail.com>
2020-05-25 22:25:41 -0400

    Added story folder.

---
+++ src/story/Emails.tsx
@@ -0,0 +1,27 @@
+import { Email } from './StoryTypes'
+
+let Emails: Email[] = [{
+  id: 0,
+  defaultAvailable: true,
+  subject: "Todays's Assignment",
+  from: "steve@awesomecorp.com",
+  to: "sid@awesomecorp.com",
+  body: "Hey Sid,\nHope you had a nice relaxing time w/ the fam in Costa Rica! We're stoked to have you back.\n\nI figured today you'd be catching up on email and whatnot so don't want to over-burden you, but if you have a second, I'm doing some analysis--could you take a look at the PTO data in X-Lerate and let me know how many TLOs were filled out last month.\n\n Best,\nSteve Cahill, CPA"
+}, {
+  id: 1,
+  defaultAvailable: true,
+  subject: "Welcome Back!",
+  from: "sarah@awesomecorp.com",
+  to: "sid@awesomecorp.com",
+  body: "Welcome back Sid!! Your trip sounded dope, hope you're not too bummed to be back on the grind.\n\nWe got a lot to catch up on." 
+}, {
+  id: 2,
+  defaultAvailable: true,
+  subject: "Welcome Back!",
+  from: "sarah@awesomecorp.com",
+  to: "sid@awesomecorp.com",
+  body: "Welcome back Sid!! Your trip sounded dope, hope you're not too bummed to be back on the grind.\n\nWe got a lot to catch up on." 
+}
+]
+
+export default Emails
---
+++ src/story/StoryTypes.tsx
@@ -0,0 +1,8 @@
+export type Email = {
+  id: number,
+  defaultAvailable: boolean,
+  subject: string,
+  from: string,
+  to: string,
+  body: string
+}
